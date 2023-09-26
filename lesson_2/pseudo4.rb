44b3f1a63fc885687f8fa8cda81da0e0c2330014
Derek Novak <derek.novak1@gmail.com>
2023-09-25 21:44:59 -0500

    refactored pseudo4

--- lesson_2/pseudo4.rb
+++ lesson_2/pseudo4.rb
@@ -4,10 +4,13 @@ Given a string.
 
 -Define a method that takes 2 arguments, a string and a given character
 -Create a new local variable, a counter, that counts down from 3
+-Create a new local variable, a string, that will contain characters from inputted string.
 -Iterate on the parameter string, character by character
+  -Push characters to new string, one at a time
   -Check if the character matches the given parameter character
     -If it does, subtract from counter
-    -If counter is 0, return the character's index value
+    -If counter is 0, break out of loop
+-Return the last index value of string if counter is 0
 -Return nil if count never reaches 0
 
 ...
@@ -19,12 +22,16 @@ Given a string
 DEFINE method 'third_occurance' (string, given character)
 
 SET counter = 3
+SET new string 
 
 ITERATE string using each
+
   IF given character = element
-    counter = counter - 1
-    IF counter == 0
-      RETURN element's index
+    new string << element
+    IF given char matches element
+      counter = counter - 1
+      break loop if counter is 0
+RETURN last index of new string if counter == 0
 RETURN nil if the counter > 0
 
 END
@@ -37,12 +44,10 @@ def third_occurance(string, given_char)
 
   string.each_char do |char|
     new_string << given_char
-    if char == given_char
-      counter -= 1
-      break if counter == 0
-    end
+    counter -= 1 if char == given_char 
+    break if counter == 0
   end
   counter == 0 ? new_string.rindex(given_char) : nil
 end
 
-puts third_occurance('axbxcdaxexa', 'a')
+puts third_occurance('axbxcdxex', 'x')
