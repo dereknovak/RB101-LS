20ea73cb203eb5eafc1937350e0e566722cd3ad1
Derek Novak <derek.novak1@gmail.com>
2023-09-30 22:46:38 -0500

    Added to mortgage files

--- lesson_2/mortgage_calculator/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator/mortgage_calculator.rb
@@ -1,6 +1,8 @@
 require 'yaml'
 MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
-loans = Hash.new
+loans_hash = Hash.new
+
+CHOICES = %w(1 2 3 4)
 
 # Methods
 
@@ -28,14 +30,19 @@ def valid_number?(input)
   integer?(input) || float?(input) 
 end
 
-def get_loan_name(hash)  # Will probably place this at end instead
+def get_choice
+  choice = gets.chomp    # CONTINUE HERE
+  case choice
+  when '1' then return '1'
+  end
+end
+
+def get_loan_name 
   loan_name = ''
   loop do
     loan_name = gets.chomp
     if valid_name?(loan_name)
-      hash[loan_name.to_sym]
-      prompt("'#{loan_name}' #{messages('loan_name')}")
-      break
+      return loan_name
     else
       prompt(messages('valid_name'))
     end
@@ -61,7 +68,7 @@ def get_apr
     num = gets.chomp
 
     if valid_number?(num)
-      num = num.to_f / 100
+      num = num.to_f / 100 / 12
       return num
     else
       prompt(messages('valid_num'))
@@ -82,4 +89,68 @@ def get_duration
   end
 end
 
-p get_duration
+def calculate_payment(loan, apr, duration)
+  payment = loan * (apr / (1 - (1 + apr)**(-duration)))
+end
+
+def yes_no
+  choice = ''
+  loop do
+    choice = gets.chomp.downcase
+    
+    if choice.start_with?('y')
+      return 'y'
+    elsif choice.start_with?('n')
+      return 'n'
+    else
+      prompt(messages('valid_choice'))
+    end
+  end
+end
+
+def save?(hash, loan, apr, duration, payment)
+  info = Array.new
+  choice = yes_no
+  if choice == 'y'
+    prompt(messages('get_loan_name'))
+    name = get_loan_name
+    prompt("'#{name}' #{messages('loan_name')}")
+    info.push(loan, apr, duration, payment)
+    hash[name.to_sym] = info
+  else
+    prompt(messages('main_menu'))
+  end
+end
+
+def loan_calculator(loans_hash)
+  prompt(messages('loan_amount'))
+  loan_amount = get_loan_amount
+  prompt(messages('apr'))
+  apr = get_apr
+  prompt(messages('duration'))
+  duration = get_duration
+  prompt(messages('calculating'))
+  sleep(2)
+  monthly_payment = calculate_payment(loan_amount, apr, duration)
+  prompt(messages('payment_script') + monthly_payment.round(2).to_s + '.')
+  sleep(2)
+  prompt(messages('save'))
+  save?(loans_hash, loan_amount, apr, duration, monthly_payment)
+  sleep(2)
+end
+
+# Program Start
+
+loop do
+  system("clear") || system("cls")
+  puts messages('welcome').center(50)
+  prompt(messages('directory'))
+  option = get_choice
+  case option
+  when '1' then loan_calculator(loans_hash)
+  when '4' then break   # NOT WORKING
+  end
+end
+
+  p loans_hash
+
