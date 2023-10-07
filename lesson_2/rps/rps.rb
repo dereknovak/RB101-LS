637b7ce3b43be240dd519df45ccf5d9cfad7c50b
Derek Novak <derek.novak1@gmail.com>
2023-10-07 18:25:57 -0500

    Completed RPS program

--- lesson_2/rps/rps.rb
+++ lesson_2/rps/rps.rb
@@ -1,102 +1,199 @@
 require 'yaml'
 MESSAGES = YAML.load_file('rps_messages.yml')
-VALID_CHOICES = %w(rock paper scissors lizard spock)
+CHALLENGER_CHOICE = %w(rock paper scissors lizard spock)
+CHOICE_SHORTHAND = %w(r p sc l sp)
+VALID_CHOICES = %w(1 2 3)
 
-winning_moves = { rock:     %w(scissors lizard),
-                  paper:    %w(rock spock),
-                  scissors: %w(paper lizard),
-                  lizard:   %w(paper spock),
-                  spock:    %w(rock scissors)
-                }
+# Methods
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def display_message(message)
-  puts MESSAGES[message]
-end
-
 def print_message(message)
-  print "=> #{message}"
+  puts MESSAGES[message]
 end
 
 def prompt_message(message)
   prompt(MESSAGES[message])
 end
 
-def get_yes_no
-  decision = ''
-  loop do
-    decision = gets.chomp.downcase
-    return 'y' if decision.start_with?('y')
-    return 'n' if decision.start_with?('n')
-    prompt_message('yes_or_no')
+def press_enter
+  prompt_message('enter')
+  gets.chomp
+end
+
+def convert_shorthand(player_choice)
+  case player_choice
+  when 'r' then 'rock'
+  when 'p' then 'paper'
+  when 'sc' then 'scissors'
+  when 'l' then 'lizard'
+  when 'sp' then 'spock'
+  else player_choice
   end
 end
 
-def get_rounds
+# Get Methods
+
+def get_choice
+  choice = ''
   loop do
-    rounds = gets.chomp.to_i
-    if rounds.odd? && rounds > 0
-      return rounds
+    choice = gets.chomp
+    if VALID_CHOICES.include?(choice)
+      return choice
+    else
+      prompt_message('valid_integer')
+    end
+  end
+end
+
+def get_total_rounds
+  loop do
+    rounds = gets.chomp.to_f
+    if rounds % 1 != 0
+      prompt_message('valid_integer')
+    elsif rounds.to_i.odd? && rounds > 0
+      return rounds.to_i
     else
       prompt_message('odd_integer')
     end
   end
 end
 
-def get_challenger
+def get_challenger(computer_choice=nil)
+  prompt_message('challenger?')
+
   loop do
-    choice = gets.chomp
-    if VALID_CHOICES.include?(choice)
+    choice = gets.chomp.downcase
+    if choice == 'cheat'
+      prompt("The computer chose '#{computer_choice}'...")
+    elsif CHALLENGER_CHOICE.include?(choice) ||
+          CHOICE_SHORTHAND.include?(choice)
+      choice = convert_shorthand(choice)
       return choice
     else
-      prompt_message('valid_choice')
+      prompt_message('valid_challenger')
     end
   end
 end
 
-def print_win?(win_hash, player_choice, computer_choice)
-  if win_hash[player_choice.to_sym].include?(computer_choice)
-    prompt('Player wins!')
+def get_result(winning_moves, player_choice, computer_choice)
+  if winning_moves[player_choice.to_sym].include?(computer_choice)
+    'player'
   elsif player_choice == computer_choice
-    prompt("It's a tie!")
+    'tie'
   else
-    prompt('Computer wins!')
+    'computer'
   end
 end
 
+def get_champion(total_rounds, player_wins, computer_wins)
+  return 'player' if player_wins > total_rounds / 2.0
+  'computer' if computer_wins > total_rounds / 2.0
+end
 
+# Display Methods
+
+def display_instructions
+  system('clear') || system('cls')
+  puts("INSTRUCTIONS\n".center(50))
+  print_message('instructions')
+  press_enter
+end
+
+def display_round(round, total_rounds, player_wins, computer_wins)
+  puts(("ROUND #{round}").center(50))
+  puts(("Player: #{player_wins} | Computer: #{computer_wins}").center(50))
+  puts(("Best of #{total_rounds}\n").center(50))
+end
+
+def display_choices(player_choice, computer_choice)
+  prompt("Player chose: '#{player_choice}'")
+  print "=> Computer chose:... "
+  sleep(2)
+  puts "'#{computer_choice}'\n\n"
+  sleep(1)
+end
+
+def display_result(result)
+  case result
+  when 'player'   then MESSAGES['player_win'].center(50)
+  when 'computer' then MESSAGES['computer_win'].center(50)
+  when 'tie'      then MESSAGES['tie'].center(50)
+  end
+end
+
+def display_champion(champion, total_rounds, player_wins, computer_wins)
+  system('clear') || system('cls')
+
+  puts(("RESULTS".center(50)))
+  puts(("Player: #{player_wins} | Computer: #{computer_wins}").center(50))
+  puts(("Best of #{total_rounds}\n").center(50))
+
+  case champion
+  when 'player'   then prompt_message('player_champion')
+  when 'computer' then prompt_message('computer_champion')
+  end
+end
+
+# Main Game Method
+
+def play_game(winning_moves)
+  prompt_message('rounds?')
+
+  total_rounds = get_total_rounds
+  round = 0
+  result = ''
+  player_wins = 0
+  computer_wins = 0
+  champion = ''
+
+  loop do
+    system('clear') || system('cls')
+
+    round += 1 unless result == 'tie'
+    display_round(round, total_rounds, player_wins, computer_wins)
+
+    computer_choice = CHALLENGER_CHOICE.sample
+    player_choice = get_challenger(computer_choice)
+    display_choices(player_choice, computer_choice)
+
+    result = get_result(winning_moves, player_choice, computer_choice)
+    puts(display_result(result))
+    case result
+    when 'player' then player_wins += 1
+    when 'computer' then computer_wins += 1
+    end
+    press_enter
+
+    champion = get_champion(total_rounds, player_wins, computer_wins)
+    break if !champion.nil?
+  end
+
+  display_champion(champion, total_rounds, player_wins, computer_wins)
+  press_enter
+end
 
 # PROGRAM START
 
-system('clear') || system('cls')
-display_message('welcome')
-prompt_message('instructions?')
-decision = get_yes_no
-display_message('instructions') if decision == 'y'
-
-
-
-prompt_message('rounds?')
-total_rounds = get_rounds
-round = 0
+winning_moves = { rock: %w(scissors lizard),
+                  paper: %w(rock spock),
+                  scissors: %w(paper lizard),
+                  lizard: %w(paper spock),
+                  spock: %w(rock scissors) }
 
 loop do
   system('clear') || system('cls')
-  round += 1
+  print_message('welcome')
+  print_message('directory')
 
-  prompt("ROUND #{round}\n\n")
-  computer_choice = VALID_CHOICES.sample
-  prompt_message('challenger?')
-  player_choice = get_challenger
- 
-  print_message("Player chose: '#{player_choice}'. Computer chose... ")
-  sleep(2)
-  puts "'#{computer_choice}'\n"
-  
-  print_win?(winning_moves, player_choice, computer_choice)
-  input = gets.chomp
-  break if round == total_rounds
+  choice = get_choice
+  case choice
+  when '1' then play_game(winning_moves)
+  when '2' then display_instructions
+  when '3' then break
+  end
 end
+
+prompt_message('goodbye')
